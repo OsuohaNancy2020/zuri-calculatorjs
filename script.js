e54a54785a4b16d82fe3ff17899f2495f3b020f3
Osuoha Chidokwa Nancy <39951626+OsuohaNancy2020@users.noreply.github.com>
2022-06-25 12:20:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var operator = prompt("operator: - / * +");
+var num1 = parseInt (prompt('num1: '))
+var num2 = parseInt (prompt('num2: '))
+
+if (operator === '+') {alert(num1+num2)}
+   else if (operator === '-') {alert(num1-num2)}
+   else if (operator === '*') {alert(num1*num2)}
+   else if (operator === '/') {alert(num1/num2)}
+    
